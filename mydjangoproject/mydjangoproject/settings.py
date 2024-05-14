4c2c2f62ae43d49426b18293a41d8b9aedfb253e
rAnto <dark_neo3000@mail.ru>
2024-05-14 03:27:38 +0300

    Models

--- mydjangoproject/mydjangoproject/settings.py
+++ mydjangoproject/mydjangoproject/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main_app'
+    'django_extensions',
+    'main_app',
 ]
 
 MIDDLEWARE = [
