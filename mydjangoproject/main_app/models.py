bc0d17bcf24721d1c0f5c635eaecd6f295ece4c3
rAnto <dark_neo3000@mail.ru>
2024-05-14 23:58:46 +0300

    ForeignKey - many-to-one

--- mydjangoproject/main_app/models.py
+++ mydjangoproject/main_app/models.py
@@ -18,6 +18,7 @@ class Posts(models.Model):
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(choices=Status.choices, default=Status.PUBLISHED)
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT)
 
     objects = models.Manager()
     published = PublishedManager()
@@ -33,3 +34,11 @@ class Posts(models.Model):
 
     def get_absolute_url(self):
         return reverse('post', kwargs={'post_slug': self.slug})
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=255, unique=True, db_index=True)
+
+    def __str__(self):
+        return self.name
