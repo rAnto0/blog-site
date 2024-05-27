2487917ef143f68bf2402c65cb471e33755e21f2
rAnto <dark_neo3000@mail.ru>
2024-05-27 13:21:06 +0300

    Custom fields and actions in the admin panel

--- mydjangoproject/main_app/admin.py
+++ mydjangoproject/main_app/admin.py
@@ -1,14 +1,29 @@
-from django.contrib import admin
+from django.contrib import admin, messages
 from .models import Posts, Category
 
 
 @admin.register(Posts)
 class PostsAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'time_create', 'is_published', 'cat')
-    list_display_links = ('id', 'title')
+    list_display = ('title', 'time_create', 'is_published', 'cat', 'brief_info')
+    list_display_links = ('title', )
     ordering = ['time_create', 'title']
     list_editable = ('is_published', )
     list_per_page = 10
+    actions = ['set_published', 'set_draft']
+
+    @admin.display(description='Краткое описание', ordering='content')
+    def brief_info(self, post: Posts):
+        return f'Описание {len(post.content)} символов.'
+
+    @admin.action(description='Опубликовать выбранные записи')
+    def set_published(self, request, queryset):
+        count = queryset.update(is_published=Posts.Status.PUBLISHED)
+        self.message_user(request, f'{count} записи опубликовано')
+
+    @admin.action(description='Снять с публикации выбранные записи')
+    def set_draft(self, request, queryset):
+        count = queryset.update(is_published=Posts.Status.DRAFT)
+        self.message_user(request, f'{count} записи сняты с публикации!', messages.WARNING)
 
 
 @admin.register(Category)
