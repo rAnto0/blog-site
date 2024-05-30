f1c558ba9c4c855d2a74fe77c10bba9d918b9d38
rAnto <dark_neo3000@mail.ru>
2024-05-30 16:03:37 +0300

    Validation of form fields. Creating a custom validator

--- mydjangoproject/main_app/models.py
+++ mydjangoproject/main_app/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinLengthValidator, MaxLengthValidator
 from django.db import models
 from django.urls import reverse
 
@@ -13,7 +14,11 @@ class Posts(models.Model):
         PUBLISHED = 1, 'Опубликовано'
 
     title = models.CharField(max_length=255, verbose_name='Заголовок')
-    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name='Slug')
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name='Slug',
+                            validators=[
+                                MinLengthValidator(5, message='Минимум 5 символов'),
+                                MaxLengthValidator(100, message='Максимум 100 символов')
+                            ])
     content = models.TextField(blank=True, verbose_name='Контент')
     time_create = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
     time_update = models.DateTimeField(auto_now=True, verbose_name='Время обновления')
