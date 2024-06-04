e07628927a389327fd2dbaddd17b4985ad8f63ca
rAnto <dark_neo3000@mail.ru>
2024-06-05 02:43:57 +0300

    ListView class

--- mydjangoproject/main_app/urls.py
+++ mydjangoproject/main_app/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('addpage/', views.AddPage.as_view(), name='addpage'),
     path('post/<slug:post_slug>/', views.show_post, name='post'),
-    path('category/<slug:cat_slug>/', views.show_category, name='category'),
-    path('tag/<slug:tag_slug>/', views.show_tag_postlist, name='tag'),
+    path('category/<slug:cat_slug>/', views.PostsCategory.as_view(), name='category'),
+    path('tag/<slug:tag_slug>/', views.PostsTagPostList.as_view(), name='tag'),
 ]
--- mydjangoproject/main_app/views.py
+++ mydjangoproject/main_app/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponseNotFound, HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
 
 from .forms import AddPostForm, UploadFileForm
 from .models import Posts, Category, TagPost, UploadFiles
@@ -16,27 +16,31 @@ menu = [
 ]
 
 
-def main_page(request):
-    posts = Posts.published.all().select_related('cat')
+# def main_page(request):
+#     posts = Posts.published.all().select_related('cat')
+#
+#     data = {
+#         'title': 'Главная страница',
+#         'menu': menu,
+#         'posts': posts,
+#         'cat_selected': 0,
+#     }
+#
+#     return render(request, 'main_app/main.html', context=data)
 
-    data = {
-        'title': 'Главная страница',
-        'menu': menu,
-        'posts': posts,
-        'cat_selected': 0,
-    }
 
-    return render(request, 'main_app/main.html', context=data)
-
-
-class MainPage(TemplateView):
+class MainPage(ListView):
+    # model = Posts
     template_name = 'main_app/main.html'
+    context_object_name = 'posts'
     extra_context = {
         'title': 'Главная страница',
         'menu': menu,
-        'posts': Posts.published.all().select_related('cat'),
         'cat_selected': 0,
     }
+
+    def get_queryset(self):
+        return Posts.published.all().select_related('cat')
 
     # def get_context_data(self, **kwargs):
     #     context = super().get_context_data(**kwargs)
@@ -73,28 +77,28 @@ def about(request):
     return render(request, 'main_app/about.html', context=data)
 
 
-def addpage(request):
-    if request.method == 'POST':
-        form = AddPostForm(request.POST, request.FILES)
-        if form.is_valid():
-            # print(form.cleaned_data)
-            # try:
-            #     Posts.objects.create(**form.cleaned_data)
-            #     return redirect('main')
-            # except:
-            #     form.add_error(None, 'Ошибка добавления поста')
-            form.save()
-            return redirect('main')
-    else:
-        form = AddPostForm()
-
-    data = {
-        'title': 'Добавить статью',
-        'menu': menu,
-        'form': form,
-    }
-
-    return render(request, 'main_app/addpage.html', context=data)
+# def addpage(request):
+#     if request.method == 'POST':
+#         form = AddPostForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             # print(form.cleaned_data)
+#             # try:
+#             #     Posts.objects.create(**form.cleaned_data)
+#             #     return redirect('main')
+#             # except:
+#             #     form.add_error(None, 'Ошибка добавления поста')
+#             form.save()
+#             return redirect('main')
+#     else:
+#         form = AddPostForm()
+#
+#     data = {
+#         'title': 'Добавить статью',
+#         'menu': menu,
+#         'form': form,
+#     }
+#
+#     return render(request, 'main_app/addpage.html', context=data)
 
 
 class AddPage(View):
@@ -141,32 +145,72 @@ def login(request):
     return render(request, 'base.html')
 
 
-def show_category(request, cat_slug):
-    category = get_object_or_404(Category, slug=cat_slug)
-    posts = Posts.published.filter(cat_id=category.pk).select_related('cat')
+# def show_category(request, cat_slug):
+#     category = get_object_or_404(Category, slug=cat_slug)
+#     posts = Posts.published.filter(cat_id=category.pk).select_related('cat')
+#
+#     data = {
+#         'title': f'Рубрика: {category.name}',
+#         'menu': menu,
+#         'posts': posts,
+#         'cat_selected': category.pk,
+#     }
+#
+#     return render(request, 'main_app/main.html', context=data)
 
-    data = {
-        'title': f'Рубрика: {category.name}',
-        'menu': menu,
-        'posts': posts,
-        'cat_selected': category.pk,
-    }
 
-    return render(request, 'main_app/main.html', context=data)
+class PostsCategory(ListView):
+    template_name = 'main_app/main.html'
+    # в html шаблоне обращение идёт к posts,
+    # если не указать этот атрибут context_object_name тогда по дефолту нужно указывать - object_list
+    context_object_name = 'posts'
+    allow_empty = False  # для отображения ошибки 404 при пустом списке
+
+    def get_queryset(self):
+        self.cat = get_object_or_404(Category, slug=self.kwargs['cat_slug'])
+        return self.cat.post.filter(is_published=1).select_related('cat')
+        # return Posts.published.filter(cat__slug=self.kwargs['cat_slug']).select_related('cat')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # cat = context['posts'][0].cat
+        context['title'] = f'Категория - {self.cat.name}'
+        context['menu'] = menu
+        context['cat_selected'] = self.cat.pk
+        return context
 
 
-def show_tag_postlist(request, tag_slug):
-    tag = get_object_or_404(TagPost, slug=tag_slug)
-    posts = tag.tags.filter(is_published=Posts.Status.PUBLISHED).select_related('cat')
+# def show_tag_postlist(request, tag_slug):
+#     tag = get_object_or_404(TagPost, slug=tag_slug)
+#     posts = tag.tags.filter(is_published=Posts.Status.PUBLISHED).select_related('cat')
+#
+#     data = {
+#         'title': f'Тег: {tag.tag}',
+#         'menu': menu,
+#         'posts': posts,
+#         'cat_selected': None,
+#     }
+#
+#     return render(request, 'main_app/main.html', context=data)
 
-    data = {
-        'title': f'Тег: {tag.tag}',
-        'menu': menu,
-        'posts': posts,
-        'cat_selected': None,
-    }
 
-    return render(request, 'main_app/main.html', context=data)
+class PostsTagPostList(ListView):
+    template_name = 'main_app/main.html'
+    # в html шаблоне обращение идёт к posts,
+    # если не указать этот атрибут context_object_name тогда по дефолту нужно указывать - object_list
+    context_object_name = 'posts'
+    allow_empty = False  # для отображения ошибки 404 при пустом списке
+
+    def get_queryset(self):
+        self.tag = get_object_or_404(TagPost, slug=self.kwargs['tag_slug'])
+        return self.tag.tags.filter(is_published=1).select_related('cat')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = f'Посты с тегом - {self.tag.tag}'
+        context['menu'] = menu
+        context['cat_selected'] = None
+        return context
 
 
 def page_not_found(request, exception):
