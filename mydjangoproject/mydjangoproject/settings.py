f84d658bb3b25ed50dcb8aa2bdab5fed159f0705
rAnto <dark_neo3000@mail.ru>
2024-06-11 20:22:12 +0300

    LoginView, LogoutView, and AuthenticationForm classes

--- mydjangoproject/mydjangoproject/settings.py
+++ mydjangoproject/mydjangoproject/settings.py
@@ -137,3 +137,6 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'main'
+LOGOUT_REDIRECT_URL = 'main'
--- mydjangoproject/users/views.py
+++ mydjangoproject/users/views.py
@@ -1,26 +1,37 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.views import LoginView
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 
 from users.forms import LoginUserForm
 
 
-def login_user(request):
-    if request.method == 'POST':
-        form = LoginUserForm(request.POST)
-        if form.is_valid():
-            cd = form.cleaned_data
-            user = authenticate(request, username=cd['username'], password=cd['password'])
-            if user and user.is_active:
-                login(request, user)
-                return HttpResponseRedirect(reverse('main'))
-    else:
-        form = LoginUserForm()
+class LoginUser(LoginView):
+    form_class = LoginUserForm
+    template_name = 'users/login.html'
+    extra_context = {'title': 'Авторизация'}
 
-    return render(request, 'users/login.html', {'form': form})
+    # def get_success_url(self):
+    #     return reverse_lazy('main')
 
 
-def logout_user(request):
-    logout(request)
-    return HttpResponseRedirect(reverse('users:login'))
+# def login_user(request):
+#     if request.method == 'POST':
+#         form = LoginUserForm(request.POST)
+#         if form.is_valid():
+#             cd = form.cleaned_data
+#             user = authenticate(request, username=cd['username'], password=cd['password'])
+#             if user and user.is_active:
+#                 login(request, user)
+#                 return HttpResponseRedirect(reverse('main'))
+#     else:
+#         form = LoginUserForm()
+#
+#     return render(request, 'users/login.html', {'form': form})
+
+
+# def logout_user(request):
+#     logout(request)
+#     return HttpResponseRedirect(reverse('users:login'))
