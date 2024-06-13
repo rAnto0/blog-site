c01e2b321caf59d52fedf7b0e3efc3785093ff8e
rAnto <dark_neo3000@mail.ru>
2024-06-13 22:41:05 +0300

    User registration using presentation functions

--- mydjangoproject/users/views.py
+++ mydjangoproject/users/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
 
-from users.forms import LoginUserForm
+from users.forms import LoginUserForm, RegisterUserForm
 
 
 class LoginUser(LoginView):
@@ -17,21 +17,15 @@ class LoginUser(LoginView):
     #     return reverse_lazy('main')
 
 
-# def login_user(request):
-#     if request.method == 'POST':
-#         form = LoginUserForm(request.POST)
-#         if form.is_valid():
-#             cd = form.cleaned_data
-#             user = authenticate(request, username=cd['username'], password=cd['password'])
-#             if user and user.is_active:
-#                 login(request, user)
-#                 return HttpResponseRedirect(reverse('main'))
-#     else:
-#         form = LoginUserForm()
-#
-#     return render(request, 'users/login.html', {'form': form})
+def register(request):
+    if request.method == 'POST':
+        form = RegisterUserForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            return render(request, 'users/register_done.html')
+    else:
+        form = RegisterUserForm()
 
-
-# def logout_user(request):
-#     logout(request)
-#     return HttpResponseRedirect(reverse('users:login'))
+    return render(request, 'users/register.html', {'form': form})
