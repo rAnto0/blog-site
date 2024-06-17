e929888bdd8f0ee9eb69767496bd4a2140091d4f
rAnto <dark_neo3000@mail.ru>
2024-06-17 17:51:13 +0300

    Authorization by e-mail. User Profile

--- mydjangoproject/mydjangoproject/settings.py
+++ mydjangoproject/mydjangoproject/settings.py
@@ -141,3 +141,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'main'
 LOGOUT_REDIRECT_URL = 'main'
 LOGIN_URL = 'users:login'
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'users.authentication.EmailAuthBackend',
+]
