d517471421c51e478b655bb6ea8c82f86938b7f6
rAnto <dark_neo3000@mail.ru>
2024-06-19 18:20:37 +0300

    Configuring the mail server using the SMTP protocol

--- mydjangoproject/mydjangoproject/settings.py
+++ mydjangoproject/mydjangoproject/settings.py
@@ -147,4 +147,14 @@ AUTHENTICATION_BACKENDS = [
     'users.authentication.EmailAuthBackend',
 ]
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_PORT = 465
+EMAIL_HOST_USER = 'blog-site-django@yandex.ru'
+EMAIL_HOST_PASSWORD = 'cpfbmepueuiytipo'
+EMAIL_USE_SSL = True
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+SERVER_EMAIL = EMAIL_HOST_USER
+EMAIL_ADMIN = EMAIL_HOST_USER
